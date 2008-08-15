2de7adbb439a5b35d25bc1185766ebecc482f2b7
kvantos <kvantos@99831fdc-644b-0410-b39c-ccabe089f53b>
2008-08-15 14:07:35 +0000

    encodig code

--- trunk/get_wether_cli.sh
+++ trunk/get_wether_cli.sh
@@ -14,7 +14,7 @@ exec tclsh "$0" ${1+"$@"}
 #<HEAT min="7" max="9"/>
 #</FORECAST>
 #
-#<patthoyts> Russian? % encoding convertfrom cp1251 [::uri::urn::unquote %CC%EE%F1%EA%E2%E0]
+#patthoyts> Russian? % encoding convertfrom cp1251 [::uri::urn::unquote %CC%EE%F1%EA%E2%E0]
 #<ijchain> <patthoyts> Москва
 #<patthoyts> The following runs through all of them: foreach e [encoding names] { puts [list $e [encoding convertfrom $e [::uri::urn::unquote %CC%EE%F1%EA%E2%E0]]] }
 #TOWN     информация о пункте прогнозирования:
